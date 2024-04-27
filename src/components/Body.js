f18fa3bdbbbb03cfd15bf036fd99380bcdcc5a01
Shivani Tiwary <shivanit.mcs17.du@gmail.com>
2024-04-28 00:05:29 +0530

    Episode 6

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,81 +1,68 @@
 import RestaurantCard from "./RestaurantCard";
-import { restaurantList } from "../utils/mockData";
-import { useState } from "react";
+// import { restaurantList } from "../utils/mockData";
+import { useState, useEffect } from "react";
+import Shimmer from "./Shimmer";
 
 //  Not using keys (not acceptable) <<<<< Index as keys <<<<<<<<<< Unique ID(best practise)
 const Body = () =>{
+    const [listOfResturants, setListOfResturants] = useState([]);
+    const [filteredResturants, setFilteredResturants] = useState([]);
+    const [searchText, setSearchText]= useState("");
 
-    const [listOfResturants, setListOfResturants] = useState(restaurantList);
+    useEffect(()=>{
+        fetchData();
+    },[])
 
-    let list = [
-        {
-            data:{
-                id: "74453",
-            name: "KFC",
-            uuid: "87727dbd-7f2b-4857-9763-359624165845",
-            city: "21",
-            area: "Athwa",
-            totalRatingsString: "1000+ ratings",
-            cloudinaryImageId: "bz9zkh2aqywjhpankb07",
-            cuisines: ["Pizzas"],
-            tags: [],
-            costForTwo: 40000,
-            costForTwoString: "₹400 FOR TWO",
-            avgRating: "3.0",
-            }
-        },
-        {    
-            data:{
-                id: "74451",
-            name: "Domino's Pizza",
-            uuid: "87727dbd-7f2b-4857-9763-359624165845",
-            city: "21",
-            area: "Athwa",
-            totalRatingsString: "1000+ ratings",
-            cloudinaryImageId: "bz9zkh2aqywjhpankb07",
-            cuisines: ["Pizzas"],
-            tags: [],
-            costForTwo: 40000,
-            costForTwoString: "₹400 FOR TWO",    
-            avgRating: "4.2",
-            }
-        },
-        {    
-            data:{
-                id: "74452",
-            name: "MCD",
-            uuid: "87727dbd-7f2b-4857-9763-359624165845",
-            city: "21",
-            area: "Athwa",
-            totalRatingsString: "1000+ ratings",
-            cloudinaryImageId: "bz9zkh2aqywjhpankb07",
-            cuisines: ["Pizzas"],
-            tags: [],
-            costForTwo: 40000,
-            costForTwoString: "₹400 FOR TWO",    
-            avgRating: "4.5",
-            }
+    const fetchData = async () => {
+        const data = await fetch("https://foodfire.onrender.com/api/restaurants?lat=21.1702401&lng=72.83106070000001&page_type=DESKTOP_WEB_LISTING");
+        const json = await data.json();
+
+              // initialize checkJsonData() function to check Swiggy Restaurant data
+      async function checkJsonData(jsonData) {
+        for (let i = 0; i < jsonData?.data?.cards.length; i++) {
+
+          // initialize checkData for Swiggy Restaurant data
+          let checkData = json?.data?.cards[i]?.card?.card?.gridElements?.infoWithStyle?.restaurants;
+
+          // if checkData is not undefined then return it
+          if (checkData !== undefined) {
+            return checkData;
+          }
         }
-    ]
+      }
+
+      // call the checkJsonData() function which return Swiggy Restaurant data
+      const resData = await checkJsonData(json);
+      setListOfResturants(resData);
+      setFilteredResturants(resData);
+      console.log(resData);
+    };
     return (
       <div className="Body">
         <div className="filter">
+            <div className="search">
+                <input type="text" className="search-box" value={searchText} onChange={(e)=>setSearchText(e.target.value)}/>
+                <button onClick={()=>{
+                    const filteredResturants = listOfResturants.filter((res) => res.info?.name.toLowerCase().includes(searchText.toLowerCase()));
+                    console.log("*******************************************Filter",filteredResturants);
+                    setFilteredResturants(filteredResturants);
+                }}>Search</button>
+            </div>
             <button 
                 className="filter-btn" 
                 onClick={()=>{
                     const filteredResturants = listOfResturants.filter(
-                        res => res.data.avgRating >4
+                        res => res.info?.avgRating > 4.2
                     );
-                    setListOfResturants(filteredResturants);
-                }}
-                >Top Rated Resturants</button>
+                    setFilteredResturants(filteredResturants);
+                }}>Top Rated Resturants
+            </button>
         </div>
         <div className="res-container">
-            {console.log(listOfResturants)}
         {listOfResturants.length>0 ? 
-          listOfResturants.map((restaurant)=>
-            (<RestaurantCard key={restaurant.data.id} resData={restaurant}/>)) 
-          : ""}
+          filteredResturants.map((restaurant)=>
+            (<RestaurantCard key={restaurant?.info?.id} resData={restaurant?.info}/>)) 
+          : <Shimmer/>}{console.log(filteredResturants)}
         </div>
 
       </div>
--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -1943,7 +1943,14 @@
   
    const RestaurantCard = (props) => {
     const { resData } = props;
-    const { name, cuisines, avgRating, costForTwo, deliveryTime, cloudinaryImageId} = resData.data;
+    const {  
+      name,
+      avgRating,
+      cuisines,
+      areaName,
+      sla,
+      costForTwo,
+      } = resData;
     // console.log(props);
     console.log(resData)
     // "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/" +
@@ -1954,9 +1961,9 @@
         {/* <img className="res-logo" alt="res-logo" src={"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/" + cloudinaryImageId}/> */}
         <h3>{name}</h3>
         <h4>{cuisines.join(", ")}</h4>
+        <h4>{areaName}</h4>
         <h4>{avgRating}</h4>
         <h4>₹{costForTwo / 100} FOR TWO</h4>
-        <h4>{deliveryTime} minutes</h4>
       </div>
     )
    }
